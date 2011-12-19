824d218b9a1728a65380fb88530a0fe12031d4b1
Siddharth Mahendraker <siddharth_mahen@me.com>
2011-12-19 13:55:48 +0200

    Tiny bug fixes
    * Also added a test; doesn't work that well tho

---
+++ test/test-multiple-attr-selectors.js
@@ -0,0 +1,34 @@
+var htmlparser = require("htmlparser2"),
+	assert = require("assert"),
+	util = require("util"),
+	soupselect = require("../lib/soupselect");
+
+var opts = {
+	ignoreWhitespace: true,
+};
+
+var fs = require("fs");
+var html = fs.readFileSync("./index.html", "utf8");
+
+var handler = new htmlparser.DefaultHandler(function(){}, opts); 
+var parser = new htmlparser.Parser(handler);
+
+parser.includeLocation = false;
+parser.parseComplete(html);
+
+var dom = handler.dom;
+var look = function(obj){
+	console.log(util.inspect(obj, false, 10));
+}
+
+
+look(soupselect.select(dom, "h2[type='awesome'][color='green']"));
+look(soupselect.select(dom, "[type='awesome'][color='blue']"));
+look(soupselect.select(dom, "[color='blue']"));
+look(soupselect.select(dom, "[class~='lol'][class~='cats']"));
+look(soupselect.select(dom, "[class~='lol'][class~='cats'][class~='asdf']"));
+console.log("Below should be blank");
+look(soupselect.select(dom, "[class='lol'][class='cats']"));
+look(soupselect.select(dom, "[class='cats']"));
+look(soupselect.select(dom, "h3[color][type='cookie']"));
+look(soupselect.select(dom, "[color][type]"));
