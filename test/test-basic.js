d8c53ad98aa081bbada433377d973a7f7e9f45d7
Siddharth Mahendraker <siddharth_mahen@me.com>
2012-01-07 16:39:55 +0200

    Completed Filter Support
    * Full support has been added for the following
    filters:
    
    - contains - empty
    - parent   - has
    - header   - not
    - eq       - gt
    - lt       - even
    - odd      - first
    - last
    
    Do note that :has() and :not() are particularly
    slow.
    
    * Updated the tests to reflect the above.
    * Aesthetic changes to package.json
    * Fixed a small bug with filters beginning in ids

--- test/test-basic.js
+++ test/test-basic.js
@@ -17,6 +17,14 @@ var li_green = [{type:'tag',name:'li',attribs:{type:'awesome',color:'green'},chi
 
 var posts = [{type:'tag',name:'p',attribs:{id:'current post',class:'main post'},children:[{data:'\n\t        \tCras mattis consectetur purus sit amet fermentum.\n\t        ',type:'text'}]},{type:'tag',name:'p',attribs:{class:'post'},children:[{data:'\n\t        \tNullam quis risus eget urna mollis ornare vel eu leo.\n\t        ',type:'text'}]}];
 
+var headers = [{type:'tag',name:'h1',children:[{data:'Testing Awesome',type:'text'}]},{type:'tag',name:'h3',attribs:{class:'subtitle'},children:[{data:'Hello, world!',type:'text'}]},{type:'tag',name:'h3',attribs:{class:'subtitle'},children:[{data:'Bonjour, monde!',type:'text'}]}];
+
+var li_parents = [{type:'tag',name:'li',children:[{data:'water',type:'text'}]},{type:'tag',name:'li',children:[{data:'sky',type:'text'}]},{type:'tag',name:'li',children:[{data:'bluebird',type:'text'}]},{type:'tag',name:'li',children:[{data:'sun',type:'text'}]},{type:'tag',name:'li',children:[{data:'daffodils',type:'text'}]},{type:'tag',name:'li',children:[{data:'leaves',type:'text'}]}];
+
+var hases = [{type:'tag',name:'ul',attribs:{color:'blue'},children:[{type:'tag',name:'li',children:[{data:'water',type:'text'}]},{type:'tag',name:'li',children:[{data:'sky',type:'text'}]},{type:'tag',name:'li',children:[{data:'bluebird',type:'text'}]}]},{type:'tag',name:'ul',attribs:{color:'yellow'},children:[{type:'tag',name:'li',children:[{data:'sun',type:'text'}]},{type:'tag',name:'li',children:[{data:'daffodils',type:'text'}]},{type:'tag',name:'li',children:[{data:'leaves',type:'text'}]}]},{type:'tag',name:'ul',children:[{type:'tag',name:'h3',attribs:{class:'subtitle'},children:[{data:'Hello, world!',type:'text'}]},{type:'tag',name:'li',attribs:{type:'awesome'},children:[{data:'I am awesome',type:'text'}]},{type:'tag',name:'li',attribs:{type:'suckish'},children:[{data:'I suck',type:'text'}]},{type:'tag',name:'h3',attribs:{class:'subtitle'},children:[{data:'Bonjour, monde!',type:'text'}]},{type:'tag',name:'li',attribs:{type:'awesome',color:'green'},children:[{data:'I am awesome, in green',type:'text'}]},{type:'tag',name:'li',attribs:{type:'cookie',color:'purple'},children:[{data:'I am a cookie, in purple',type:'text'}]},{type:'tag',name:'li',attribs:{type:'pizza',color:'green'},children:[{data:'I am a pizza, in green',type:'text'}]}]}];
+
+var nots = [{type:'tag',name:'div',attribs:{id:'nav'},children:[{type:'tag',name:'ul',attribs:{color:'blue'},children:[{type:'tag',name:'li',children:[{data:'water',type:'text'}]},{type:'tag',name:'li',children:[{data:'sky',type:'text'}]},{type:'tag',name:'li',children:[{data:'bluebird',type:'text'}]}]},{type:'tag',name:'ul',attribs:{color:'yellow'},children:[{type:'tag',name:'li',children:[{data:'sun',type:'text'}]},{type:'tag',name:'li',children:[{data:'daffodils',type:'text'}]},{type:'tag',name:'li',children:[{data:'leaves',type:'text'}]}]}]},{type:'tag',name:'ul',attribs:{color:'blue'},children:[{type:'tag',name:'li',children:[{data:'water',type:'text'}]},{type:'tag',name:'li',children:[{data:'sky',type:'text'}]},{type:'tag',name:'li',children:[{data:'bluebird',type:'text'}]}]},{type:'tag',name:'ul',attribs:{color:'yellow'},children:[{type:'tag',name:'li',children:[{data:'sun',type:'text'}]},{type:'tag',name:'li',children:[{data:'daffodils',type:'text'}]},{type:'tag',name:'li',children:[{data:'leaves',type:'text'}]}]}];
+
 suite("#select()", function(){
 	test("should return [] when not present", function(){
 		assert.deepEqual([], select(dom, "does_not_exist"));
@@ -60,15 +68,23 @@ suite("#select()", function(){
         assert.deepEqual([ul_blue[0].children[0]], select(dom, "ul[color='blue'] li:contains('water')"));
         assert.deepEqual([ul_blue[0].children[2]], select(dom, "#nav ul[color='blue] li:last"));
         assert.deepEqual([ul_blue[0].children[2]], select(dom, "#nav ul:first li:last"));
-        assert.deepEqual([], select(dom, "#nav ul:empty"));
 
-        /*
-        assert.deepEqual( ,select(dom, "#nav li:lt(3)"));
-        assert.deepEqual( ,select(dom, "#nav li:eq(3)"));
-        assert.deepEqual( ,select(dom, "#nav li:gt(3)"));
-        assert.deepEqual( ,select(dom, "#nav li:even"));
-        assert.deepEqual( ,select(dom, "#nav li:odd"));
-        */
+        assert.deepEqual([ul_blue[0].children[0]], select(dom, "#nav li:lt(1)"));
+        assert.deepEqual([ul_blue[0].children[2]], select(dom, "#nav li:eq(2)"));
+        assert.deepEqual([ul_blue[0].children[2]], select(dom, "#nav ul[color='blue'] li:gt(1)"));
+        assert.deepEqual([ul_blue[0].children[1]], select(dom, "#nav ul[color='blue'] li:even"));
+        assert.deepEqual([ul_blue[0].children[0]].concat([ul_blue[0].children[2]]), select(dom, "#nav ul[color='blue'] li:odd"));
+
+        assert.deepEqual(headers, select(dom, ":header"));
+        assert.deepEqual(headers, select(dom, "html:header"));
+        assert.deepEqual([], select(dom, "#nav ul:empty"));
+        assert.deepEqual(li_parents, select(dom, "#nav li:parent"));
+
+        // The following calls are enterily responsible for the ~150ms times
+        assert.deepEqual(hases, select(dom, ":has(li)"));
+        assert.deepEqual(hases, select(dom, "html:has(li)"));
+        assert.deepEqual(nots, select(dom, "#nav:not(li)"));
+        assert.deepEqual(nots, select(dom, "#nav :not(li)"));
      });
      test("should return proper elements based on nested filter selectors", function(){
         assert.deepEqual(dom, select(dom, ":odd(:first)"));
